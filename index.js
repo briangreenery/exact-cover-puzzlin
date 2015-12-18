08b7355417ae42ed66f6c60fcb90d27ec71dbea1
Brian Green <briangreenery@gmail.com>
2015-12-17 17:53:55 -0800

    generate cover matrix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-function spaceFor(row, index) {
+function sizeWithBlank(row, index) {
   var space = row[index];
   
   if (index !== row.length - 1) {
@@ -10,12 +10,12 @@ function spaceFor(row, index) {
   return space;
 }
 
-function totalSpace(row, index) {
+function sizeWithTrailing(row, index) {
   if (index === row.length - 1) {
-    return spaceFor(row, index);
+    return sizeWithBlank(row, index);
   }
   
-  return totalSpace(row, index + 1) + spaceFor(row, index);
+  return sizeWithTrailing(row, index + 1) + sizeWithBlank(row, index);
 }
 
 function parseInput() {
@@ -27,20 +27,20 @@ function parseInput() {
     return row.map(function(block, index) {
       return {
         size: row[index], 
-        spaceFor: spaceFor(row, index),
-        totalSpace: totalSpace(row, index) 
+        sizeWithBlank: sizeWithBlank(row, index),
+        sizeWithTrailing: sizeWithTrailing(row, index) 
       };
     });
   });
   
-  input.columns = input.columns.map(function(column) {
+  input.cols = input.columns.map(function(column) {
     return column.split(' ').map(Number);
   }).map(function(col) {
     return col.map(function(block, index) {
       return {
         size: col[index],
-        spaceFor: spaceFor(col, index),
-        totalSpace: totalSpace(col, index) 
+        sizeWithBlank: sizeWithBlank(col, index),
+        sizeWithTrailing: sizeWithTrailing(col, index) 
       };
     });
   })
@@ -48,57 +48,134 @@ function parseInput() {
   return input;
 }
 
-function makeBoard(size) {
-  var board = [];
+function Cover(input) {
+  var rows = [];
+  var cols = [];
+  var squares = [];
   
-  for (var i = 0; i < input.size; i += 1) {
-    var columns = [];
-    for (var j = 0; j < input.size; j += 1) {
-      columns.push(0);
-    }
-    
-    board.push(columns);
+  var matrix = [];
+  var choices = [];
+  
+  function addConstraint(name) {
+    matrix.push({name: name, choices: []});
+    return matrix.length - 1;
   }
   
-  return board;
+  function getSquareConstraint(x, y) {
+    return matrix[squares[x + input.size * y]];
+  }
+  
+  function init() {
+    var i, j;
+    
+    for (i = 0; i < input.size; i += 1) {
+      rows.push(addConstraint('row ' + i));
+    }
+    
+    for (i = 0; i < input.size; i += 1) {
+      cols.push(addConstraint('col ' + i));
+    }
+    
+    for (i = 0; i < input.size; i += 1) {
+      for (j = 0; j < input.size; j += 1) {
+        squares.push(addConstraint('square ' + j + ', ' + i));
+      }
+    }
+  }
+ 
+  var methods = {};
+  
+  methods.addRowChoice = function(row, squares) {
+    // Add the choice.
+    choices.push({name: 'row ' + row + ': ' + squares.join('')});
+    var choice = choices.length - 1;
+    
+    // Fulfill the row constraint.
+    matrix[rows[row]].choices.push(choice);
+    
+    // For each *filled* square, fulfill the square constraint.
+    for (var x = 0; x < input.size; x += 1) {
+      if (squares[x] === 1) {
+        getSquareConstraint(x, row).choices.push(choice);
+      }
+    }
+  };
+  
+  methods.addColChoice = function(col, squares) {
+    // Add the choice.
+    choices.push({name: 'col ' + col + ': ' + squares.join('')});
+    var choice = choices.length - 1;
+    
+    // Fulfill the col constraint.
+    matrix[cols[col]].choices.push(choice);
+    
+    // For each *unfilled* square, fulfill the square constraint.
+    for (var y = 0; y < input.size; y += 1) {
+      if (squares[y] === 0) {
+        getSquareConstraint(col, y).choices.push(choice);
+      }
+    }
+  };
+  
+  methods.debug = function() {
+    console.log(JSON.stringify(matrix, null, '  '));
+  };
+  
+  init();
+  return methods;
 }
 
 var input = parseInput();
+var cover = Cover(input);
 
-// console.log(makeBoard(input.size));
-// console.log(JSON.stringify(input));
-
-function set(arr, start, size) {
+function setSquares(squares, start, size) {
   for (var i = start; i < start + size; i += 1) {
-    arr[i] = 1;
+    squares[i] = 1;
   }
 }
 
-function clear(arr, start, size) {
+function clearSquares(squares, start, size) {
   for (var i = start; i < start + size; i += 1) {
-    arr[i] = 0;
+    squares[i] = 0;
   }
 }
 
-function foo(row, index, start, marks) {
-  if (index === row.length) {
-    console.log(marks);
-    return;
+function enumChoices(blocks, index, start, squares, fn) {
+  if (index === blocks.length) {
+    return fn(squares);
   }
   
-  var block = row[index];
+  var block = blocks[index];
   
-  for (var i = start; i <= input.size - block.totalSpace; i += 1) {
-    set(marks, i, block.size);
-    foo(row, index + 1, i + block.spaceFor, marks);
-    clear(marks, i, block.size);
+  for (var i = start; i <= input.size - block.sizeWithTrailing; i += 1) {
+    setSquares(squares, i, block.size);
+    enumChoices(blocks, index + 1, i + block.sizeWithBlank, squares, fn);
+    clearSquares(squares, i, block.size);
   }
 }
 
-var marks = [];
-for (var i = 0; i < 25; i += 1) {
-  marks.push(0);
+function initSquares() {
+  var squares = [];
+  
+  for (var i = 0; i < input.size; i += 1) {
+    squares.push(0);
+  }
+  
+  return squares;
 }
 
-console.log(input.rows[0]);
-foo(input.rows[0], 0, 0, marks);
+var squares = initSquares();
+
+input.rows.forEach(function(block, index) {
+  enumChoices(block, 0, 0, squares, function(squares) {
+    cover.addRowChoice(index, squares);
+  });
+});
+
+input.cols.forEach(function(block, index) {
+  enumChoices(block, 0, 0, squares, function(squares) {
+    cover.addColChoice(index, squares);
+  });
+});
+
+cover.debug();
